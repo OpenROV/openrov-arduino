f74e33cb3d5eb0027f3b1d107224e6623e7f3f59
Charles Cross <charles@openrov.com>
2016-02-04 14:49:57 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+mkdir -p output/opt/openrov/
+
+cd output/opt/openrov
+
+// Get the Arduino IDE
+git clone https://github.com/arduino/Arduino.git arduino
+cd arduino
+git checkout 36c94191183600440676967d996abff8dbcd149d
+
+# Remove IDE files, we just want the core files and libraries
+rm -rf app/ arduino-core/ build/ .classpath .project README.md examples_formatter.conf format.every.sketch.sh lib_sync
+
+# Remove the sam core files. We don't need these.
+cd hardware/arduino
+rm -rf sam
+cd ..
+
+# Get the OpenROV cores
+git clone https://github.com/OpenROV/OROV-ArduinoCores.git openrov
+
+# Setup arduino tools
+cd tools
+
+# Build CTAGS
+git clone https://github.com/arduino/ctags.git
+cd ctags
+./configure
+make -j8
+cp ctags ../ctags2
+cd ..
+rm -rf ctags/
+mv ctags2 ctags
+
+# Build bossac
+git clone https://github.com/spiderkeys/BOSSA.git
+cd BOSSA
+make
+cp bin/bossac ..
+cd ..
+rm -rf BOSSA
+
+# Build openocd
+# TODO
+  
+# Download Atmel's CMSIS
+wget http://downloads.arduino.cc/CMSIS-4.0.0.tar.bz2
+tar xfj CMSIS-4.0.0.tar.bz2
+rm CMSIS-4.0.0.tar.bz2
+
+
+
---
+++ package-deb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+#Install Pre-req
+gem install fpm
+export DIR=${PWD}
+export PACKAGE="openrov-arduino"
+
+
+ARCH=`uname -m`
+if [ ${ARCH} = "armv7l" ]
+then
+  ARCH="armhf"
+fi
+
+./build.sh
+
+export PACKAGE_VERSION=1.0.0-1~${BUILD_NUMBER}
+
+#package
+cd $DIR
+
+fpm -f -m info@openrov.com -s dir -t deb -a $ARCH \
+	-n ${PACKAGE} \
+	-v ${PACKAGE_VERSION} \
+	--description "OpenROV Arduino core files and tools" \
+	-C ${DIR}/output ./
